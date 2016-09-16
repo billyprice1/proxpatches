cb8a0eb9557d3d8bb456f6ca0192f84da267dd05
Jean-Francois Dagenais <dagenaisj@sonatest.com>
2016-09-15 22:49:38 -0400

    add znapzend and all setup to apply-permanent-patches.sh

--- apply-permanent-patches.sh
+++ apply-permanent-patches.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+set -e
+
+rm -fiv /etc/apt/sources.list.d/pve-enterprise.list
+rsync -axvP files/ /
+
+if ! date | grep -q EDT
+then
+    dpkg-reconfigure tzdata locales
+fi
+
+apt-get update
+apt-get dist-upgrade
+
+
 apt-get install -y \
 htop \
 quilt \
@@ -8,6 +22,31 @@ tree \
 ncdu \
 pv \
 
-rsync -axvP files/ /
+znapzendVer=0.15.7
 
+if [ ! -e /opt/znapzend-$znapzendVer ]
+then
+{
+    TMPDIR=$(mktemp -d)
+    trap 'rm -rf $TMPDIR' EXIT
+    
+    rm -rf /opt/znapzend-*
+    
+    cd $TMPDIR
+    wget https://github.com/oetiker/znapzend/releases/download/v$znapzendVer/znapzend-$znapzendVer.tar.gz
+    tar zxvf znapzend-$znapzendVer.tar.gz
+    cd znapzend-$znapzendVer
+    ./configure --prefix=/opt/znapzend-$znapzendVer
+
+    make install
+    
+    for x in /opt/znapzend-$znapzendVer/bin/*
+    do
+        ln -s $x /usr/local/bin
+    done
+    
+    cd -
+}
+fi
+    
 ./post-apt-patch.sh
